e839e14a245797eede6d42766793ee63be0916c2
missock237-spec <missock237@gmail.com>
2026-09-14 19:02:29 +0100

    fix(tools): register artifact and ZIP tools

--- lib/tools/default-registry.ts
+++ lib/tools/default-registry.ts
@@ -1,81 +1,17 @@
-import {
-  ToolRegistry,
-} from "./registry";
+import { ToolRegistry } from "./registry";
+import { webSearchTool } from "./web/search";
+import { webOpenTool } from "./web/open";
+import { getArtifactTool } from "./files/get-artifact";
+import { createArtifactTool } from "./files/create-artifact";
+import { createZipTool } from "./files/create-zip";
+import { analyzeZipTool } from "./files/analyze-zip";
+import { createComposioTool } from "@/lib/integrations/composio/adapter";
+import { githubCreateRepositoryTool } from "@/lib/integrations/github/tools";
 
-import {
-  webSearchTool,
-} from "./web/search";
-
-import {
-  webOpenTool,
-} from "./web/open";
-
-import {
-  getArtifactTool,
-} from "./files/get-artifact";
-
-registry.register(
-  getArtifactTool,
-);
-
-import {
-  createComposioTool,
-} from "@/lib/integrations/composio/adapter";
-
-import {
-  githubCreateRepositoryTool,
-} from "@/lib/integrations/github/tools";
-
-registry.register(
-  createArtifactTool,
-);
-
-registry.register(
-  analyzeZipTool,
-);
-
-registry.register(
-  createZipTool,
-);
-
-import {
-  createArtifactTool,
-} from "./files/create-artifact";
-
-import {
-  analyzeZipTool,
-} from "./files/analyze-zip";
-
-import {
-  createZipTool,
-} from "./files/create-zip";
-
-export function createDefaultToolRegistry() {
-  const registry =
-    new ToolRegistry();
-
-  registry.register(
-    webSearchTool,
-  );
-
-  registry.register(
-    webOpenTool,
-  );
-
-  if (
-    process.env.COMPOSIO_API_KEY
-  ) {
-    registry.register(
-      createComposioTool(),
-    );
-  }
-
-  if (
-  process.env.GITHUB_TOKEN
-) {
-  registry.register(
-    githubCreateRepositoryTool,
-  );
-}
+export function createDefaultToolRegistry(): ToolRegistry {
+  const registry = new ToolRegistry();
+  for (const tool of [webSearchTool, webOpenTool, getArtifactTool, createArtifactTool, createZipTool, analyzeZipTool]) registry.register(tool);
+  if (process.env.COMPOSIO_API_KEY) registry.register(createComposioTool());
+  if (process.env.GITHUB_TOKEN) registry.register(githubCreateRepositoryTool);
   return registry;
 }
