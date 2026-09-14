64072b43dda4f2d079ad60c4b59b491b78c93130
missock237-spec <missock237@gmail.com>
2026-09-14 19:04:04 +0100

    feat(tools): register file.create execution tool

--- lib/tools/default-registry.ts
+++ lib/tools/default-registry.ts
@@ -3,6 +3,7 @@ import { webSearchTool } from "./web/search";
 import { webOpenTool } from "./web/open";
 import { getArtifactTool } from "./files/get-artifact";
 import { createArtifactTool } from "./files/create-artifact";
+import { createFileTool } from "./files/create";
 import { createZipTool } from "./files/create-zip";
 import { analyzeZipTool } from "./files/analyze-zip";
 import { createComposioTool } from "@/lib/integrations/composio/adapter";
@@ -10,7 +11,7 @@ import { githubCreateRepositoryTool } from "@/lib/integrations/github/tools";
 
 export function createDefaultToolRegistry(): ToolRegistry {
   const registry = new ToolRegistry();
-  for (const tool of [webSearchTool, webOpenTool, getArtifactTool, createArtifactTool, createZipTool, analyzeZipTool]) registry.register(tool);
+  for (const tool of [webSearchTool, webOpenTool, getArtifactTool, createArtifactTool, createFileTool, createZipTool, analyzeZipTool]) registry.register(tool);
   if (process.env.COMPOSIO_API_KEY) registry.register(createComposioTool());
   if (process.env.GITHUB_TOKEN) registry.register(githubCreateRepositoryTool);
   return registry;
